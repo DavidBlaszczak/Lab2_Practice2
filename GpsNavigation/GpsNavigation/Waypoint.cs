42eff57adf3a9c784f6eaff850373df7ee56a80d
DavidBlaszczak <blaszczakdavid26@gmail.com>
2025-06-16 18:52:51 +0200

    Solution

--- GpsNavigation/GpsNavigation/Waypoint.cs
+++ GpsNavigation/GpsNavigation/Waypoint.cs
@@ -2,5 +2,24 @@
 
 public class Waypoint
 {
-    //Your code here
+    public string Name { get; }
+
+    public (double Latitude, double Longitude) Coordinates { get; }
+
+    public Waypoint(string name, double latitude, double longitude)
+    {
+        Name = name;
+        Coordinates = (latitude, longitude);
+    }
+
+    public (double, double) GetCoordinates()
+    {
+        return (Coordinates.Latitude, Coordinates.Longitude);
+    }
+
+    public void Deconstruct(out double latitude, out double longitude)
+    {
+        latitude = Coordinates.Latitude;
+        longitude = Coordinates.Longitude;
+    }
 }
